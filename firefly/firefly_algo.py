c1ce3ff915dc39482fe37a614531a7028518f6fd
Vineet Mahajan <vineetmahajan2000@gmail.com>
2022-04-10 16:39:45 +0530

    Created main algo

--- firefly/firefly_algo.py
+++ firefly/firefly_algo.py
@@ -1,8 +1,30 @@
+from math import gamma
 import numpy as np
-from typing import List
+from typing import List, Callable
+from copy import deepcopy
 
 from .firefly import FireFly
 
 
 class FireflyOptimizer:
-    pass
+
+    def __init__(self, **kwargs) -> None:
+        self.max_irterations: int = kwargs.get("max_irterations", 20)
+        self.fireflies_num: int = kwargs.get("fireflies_num", 100)
+        self.__fireflies = [FireFly(**kwargs) for _ in range(self.fireflies_num)]
+
+    def optimize(self) -> List[FireFly]:
+        best = deepcopy(max(self.__fireflies))
+        for _ in range(self.max_irterations):
+            for firefly in self.__fireflies:
+                for targate_firefly in self.__fireflies:
+                    if firefly < targate_firefly:
+                        firefly.move_towards(targate_firefly)
+
+            temp_best = max(self.__fireflies)
+            if best < temp_best:
+                best = deepcopy(temp_best)
+
+            temp_best.position += temp_best.random_targate()
+
+        return self.__fireflies
